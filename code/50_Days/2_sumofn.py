f15fab3807947ca42e717e9c1caa862c4856e3e7
Prakash Thirunavukkarasu <innovation.prakash@gmail.com>
2022-01-27 20:02:29 -0500

    done with 2sumofn
    '
    
    '

--- code/50_Days/2_sumofn.py
+++ code/50_Days/2_sumofn.py
@@ -1,14 +1,16 @@
 
-# Ask usr for a number n 
+# Ask usr for a number  
 # return the sumation
 # ex user enters 4. you need to return 4 + 3 + 2 + 1 + 0, which is 10
 
 print('------------- START ----------------')
-xstr =input("enter your age ")
+xstr =input("enter a number ")
 xnum= int(xstr)
-sum = 0
-while(xnum>0):
+sum=xnum
+x=xnum
+while(x>0):
+    x=x-1
+    sum=sum+x   
+print("The sum is : ", sum)
 
-
-print(sum)
 print('------------- END ----------------')
